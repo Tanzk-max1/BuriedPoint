a83c6972f8f7173c6acbd5c3d34395ad8dc5c568
zk_glb <1552535580@qq.com>
2024-05-15 21:05:36 +0800

    first commit

---
+++ BuriedPoint/include/buried.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdint.h>
+
+#define BURIED_EXPORT __declspec(dllexport)
+
+
+
+extern "C"{
+BURIED_EXPORT int BuriedTest();
+
+typedef struct Buried Buried;
+
+struct BuriedConfig 
+{
+    /* data */
+    const char* host;
+    const char* topic;
+    const char* user_id;
+    const char* version;
+    const char* custom_data;
+
+};
+
+BURIED_EXPORT Buried* Buried_Create(const char* work_dir);
+
+BURIED_EXPORT void Buried_Destroy(Buried* buried);
+
+BURIED_EXPORT int32_t Buried_Start(Buried* buried, BuriedConfig* config);
+
+BURIED_EXPORT int32_t Buried_Report(Buried* buried, const char* report_data,
+                                    uint32_t priority);
+
+}
+
---
+++ BuriedPoint/src/buried_core.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdint.h>
+#include <filesystem>
+
+#include <memory.h>
+
